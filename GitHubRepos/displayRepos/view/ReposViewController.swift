12ba70df42a801218307b4fd4bc9c6c4cbbbe0ca
Ahmed <ahmed.sharf.777@gmail.com>
2021-11-19 04:01:35 +0200

    finish UI and logic of searching

--- GitHubRepos/displayRepos/view/ReposViewController.swift
+++ GitHubRepos/displayRepos/view/ReposViewController.swift
@@ -14,8 +14,13 @@ class ReposViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
     
-    var reposArray = ["1", "2", "3", "4", "5"]
+    var searchController: UISearchController!
     
+    var reposArray = ["1", "356", "3", "2035698", "7705356"]
+    var filteredData = [String]()
+    var isFiltering: Bool {
+        return searchController.isActive && searchController.searchBar.text?.count ?? 2 > 1
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +29,15 @@ class ReposViewController: UIViewController {
         //register cell
         let nibCell = UINib(nibName: Constants.reposCellId, bundle: nil)
         tableView.register(nibCell, forCellReuseIdentifier: Constants.reposCellId)
+        
+        //search controller
+        searchController = UISearchController(searchResultsController: nil)
+        searchController.searchBar.placeholder = "Search Repos"
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        definesPresentationContext = true
+        navigationItem.titleView = searchController.searchBar
+        searchController.hidesNavigationBarDuringPresentation = false
     }
 
 
@@ -31,15 +45,25 @@ class ReposViewController: UIViewController {
 
 extension ReposViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if isFiltering {
+            return filteredData.count
+        }
         return reposArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: Constants.reposCellId, for: indexPath) as! ReposTableViewCell
         
-        cell.repoNameLabel.text = reposArray[indexPath.row]
-        cell.ownerNameLabel.text = reposArray[indexPath.row]
-        cell.creationDateLabel.text = reposArray[indexPath.row]
+        let repo: String
+        if isFiltering {
+            repo = filteredData[indexPath.row]
+        } else {
+          repo = reposArray[indexPath.row]
+        }
+
+        cell.repoNameLabel.text = repo
+        cell.ownerNameLabel.text = repo
+        cell.creationDateLabel.text = repo
         
         return cell
     }
@@ -49,3 +73,15 @@ extension ReposViewController: UITableViewDataSource, UITableViewDelegate {
     }
 }
 
+extension ReposViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        if let searchText = searchController.searchBar.text {
+            if searchText.count > 1 {
+                filteredData = reposArray.filter({(dataString: String) -> Bool in
+                    return dataString.lowercased().contains(searchText.lowercased())
+                })
+            }
+            tableView.reloadData()
+        }
+    }
+}
